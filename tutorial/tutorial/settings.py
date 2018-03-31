2d3388c323a46e5c7876142921cb7f6c3eaef108
shimomura <shimomura@usa-mimi.jp>
2018-03-31 17:34:01 +0900

    rest_frameworkインストール，設定
    `pip install djangorestframework` でインストール，settings.pyのINSTALLED_APPに追加

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'polls',
 ]
 
@@ -104,9 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
