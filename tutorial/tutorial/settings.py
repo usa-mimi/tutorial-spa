3ee9bcf2f1de419d3d027462d58158a591d8bdd5
shimomura <shimomura@usa-mimi.jp>
2018-04-05 01:30:04 +0900

    ページネーション処理追加

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = '/static/'
 # CORS設定
 # ==========
 CORS_ORIGIN_ALLOW_ALL = True
+
+
+# =========================
+# django-restframework 設定
+# =========================
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+}
