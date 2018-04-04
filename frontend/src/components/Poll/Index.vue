ad2db08c4def64d97bda4a9174dfd871598d50f9
shimomura <shimomura@usa-mimi.jp>
2018-04-05 00:52:30 +0900

    ページを開いた時に質問一覧を取得するように修正 （CORSエラーでまだ動かない）
    `$ npm install axios --save` で通信用のライブラリを用意
    PollIndexに取得用のメソッドを用意し，ページを開いた時に実行するように修正

--- frontend/src/components/Poll/Index.vue
+++ frontend/src/components/Poll/Index.vue
@@ -5,7 +5,19 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'PollIndex',
+  methods: {
+    fetchData () {
+      axios.get('http://localhost:8000/api/1.0/questions/').then(res => {
+        console.log(res)
+      })
+    },
+  },
+  mounted () {
+    this.fetchData()
+  },
 }
 </script>
