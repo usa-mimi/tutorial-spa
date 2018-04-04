3124f4b26039e3058a60c93ca90792416ab89905
shimomura <shimomura@usa-mimi.jp>
2018-04-05 01:40:03 +0900

    APIの戻り値の変数名をキャメルケースに修正

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -137,4 +137,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
 }
