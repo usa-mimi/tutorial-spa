5592d25f5633613c436d274e769bd969fc918c00
shimomura <shimomura@usa-mimi.jp>
2018-04-05 12:10:30 +0900

    カード形式で質問内容を並べて表示

--- frontend/src/components/Poll/Index.vue
+++ frontend/src/components/Poll/Index.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    polls
+    <v-card>
+      <v-container grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs6 v-for="data in questions" :key="data.id">
+            <v-card color="primary" class="white--text">
+              <v-card-title primary-title>
+                <div>{{ data.questionText }}</div>
+              </v-card-title>
+              <v-card-text>
+                <div>{{ data.pubDate }}</div>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
   </div>
 </template>
 
@@ -9,10 +24,15 @@ import axios from 'axios'
 
 export default {
   name: 'PollIndex',
+  data () {
+    return {
+      questions: [],
+    }
+  },
   methods: {
     fetchData () {
       axios.get('http://localhost:8000/api/1.0/questions/').then(res => {
-        console.log(res)
+        this.questions = res.data.results
       })
     },
   },
