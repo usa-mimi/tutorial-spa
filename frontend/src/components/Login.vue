0b0dc4a12b3d1e8f222c7d2cc29dbb9420135299
shimomura <shimomura@usa-mimi.jp>
2018-05-16 17:36:42 +0900

    ログインコンポーネントの追加とURL設定

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    loginページ
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import PollIndex from '@/components/Poll/Index'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'PollIndex',
       component: PollIndex,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
     },
   ],
 })
