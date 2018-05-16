e66c3a8f20fc76b6680e6e835639e4940f08584e
shimomura <shimomura@usa-mimi.jp>
2018-05-16 18:09:54 +0900

    ログインID，パスワード入力フォームとログインボタン追加

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    loginページ
+    <v-form>
+      <v-text-field
+        v-model="username"
+        label="ログインID"
+        required></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="パスワード"
+        required></v-text-field>
+      <v-btn @click="submit">ログイン</v-btn>
+    </v-form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
+  data () {
+    return {
+      username: null,
+      password: null,
+    }
+  },
+  methods: {
+    submit () {
+      alert('login')
+    },
+  },
 }
 </script>
