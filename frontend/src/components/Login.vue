37e6f63c52bc5bbf66079a1353f363d8057b2ee1
shimomura <shimomura@usa-mimi.jp>
2018-05-17 12:12:12 +0900

    エラー時の処理，パスワードの表示／非表示処理追加

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <v-form>
+    <v-alert :value="nonFieldErrors.length" type="error">
+      <div v-for="error in nonFieldErrors" :key="error">{{ error }}</div>
+    </v-alert>
+    <v-form v-model="valid">
       <v-text-field
         v-model="username"
         label="ログインID"
+        :rules="usernameRules"
         required></v-text-field>
       <v-text-field
         v-model="password"
         label="パスワード"
+        :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+        :append-icon-cb="() => (showPassword = !showPassword)"
+        :type="showPassword ? 'text' : 'password'"
+        :rules="passwordRules"
         required></v-text-field>
-      <v-btn @click="submit">ログイン</v-btn>
+      <v-btn :disabled="!valid" @click="submit">ログイン</v-btn>
     </v-form>
   </div>
 </template>
@@ -19,14 +27,26 @@ export default {
   name: 'Login',
   data () {
     return {
+      valid: false,
       username: null,
       password: null,
+      showPassword: false,
+      nonFieldErrors: [],
+      usernameRules: [
+        v => !!v || 'ログインIDを入力してください',
+      ],
+      passwordRules: [
+        v => !!v || 'パスワードを入力してください',
+      ],
     }
   },
   methods: {
     submit () {
+      this.nonFieldErrors = []
       this.$request.auth.login(this.username, this.password).then(res => {
         console.log(res.data)
+      }, err => {
+        this.nonFieldErrors = err.response.data.nonFieldErrors
       })
     },
   },
